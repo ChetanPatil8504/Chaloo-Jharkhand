bffc45d4aabdd7b834783cef67ed25c8e21e2431
ChetanPatil8504 <cp0875556@gmail.com>
2026-08-11 23:38:53 +0530

    Initial project setup

---
+++ backend/src/main/java/com/project/Chaloo/Jharkhand/ChalooJharkhandApplication.java
@@ -0,0 +1,13 @@
+package com.project.Chaloo.Jharkhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalooJharkhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalooJharkhandApplication.class, args);
+	}
+
+}
